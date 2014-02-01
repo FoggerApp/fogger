33fa32b840d576cf8ca1dcefa94ff69dca963870
simdugas <simdugas@gmail.com>
2014-02-01 16:51:05 -0400

    added a marker to the map

--- static/js/map.js
+++ static/js/map.js
@@ -1,13 +1,78 @@
 $(function(){
+  /* init map */
+  var map = null;
+
+  /* set the width of the map function */
+  var reDim = function() {
+    $('#map-canvas').width($('#map-canvas').width());
+    $('#map-canvas').height(window.innerHeight - $('.navbar').height());
+  }
+
+  /* request user location function */
+  var getLocation = function(pass) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(pos){
+        var uloc = new google.maps.LatLng(pos.coords.latitude,
+                                          pos.coords.longitude);
+        pass(uloc);
+      });
+    } else {
+      alert("Could not get user location!");
+    }
+  };
+
+  var panToUserLoc = function() { 
+    getLocation(function(uloc){
+      if(map != null) {
+        map.panTo(uloc);
+      } else {
+        return null;
+      }
+    });
+  };
+  
+  var placeUserMarker = function() {
+    getLocation(function(uloc){
+      console.log("Placing marker", uloc, "on", map);
+      if(map != null) {
+        var userMarker = new google.maps.Marker({
+          position: uloc,
+          map: map,
+          title: 'You'
+          });
+      }
+    });
+  };
+
+  var getUserMarker = function() {
+    
+  };
+
+  /* Map Initialization Function */
   function initialize() {
     var mapOptions = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 8
+      zoom: 17,
+      panControl: false,
+      zoomControl: false,
+      streetViewControl: false,
+      scaleControl: false,
+      rotateControl: false,
+      overviewMapControl: false,
+
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
+    reDim();
+    map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+    google.maps.event.addDomListener(map, 'idle', 
+            function(){console.log(map.getBounds())});
+
     }
 
+  /* Events */
+  
   google.maps.event.addDomListener(window, 'load', initialize);
+  
+  panToUserLoc();
+  placeUserMarker();
 });
 
