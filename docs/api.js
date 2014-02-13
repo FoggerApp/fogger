80664b76055c63cac4b78110f081b9bd720702d0
sdugas <sdugas@centi.cs.dal.ca>
2014-02-13 11:05:48 -0400

    committed docs

---
+++ docs/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "navigator"
+    ],
+    "modules": [
+        "fogger",
+        "navigator"
+    ],
+    "allModules": [
+        {
+            "displayName": "fogger",
+            "name": "fogger"
+        },
+        {
+            "displayName": "navigator",
+            "name": "navigator",
+            "description": "Provides navigator functionality and simulates\nuser movement."
+        }
+    ]
+} };
+});
