e3b4ade3657289697f434da74a6a71ab72d36621
sdugas <sdugas@centi.cs.dal.ca>
2014-02-13 12:21:40 -0400

    docs update

--- docs/api.js
+++ docs/api.js
@@ -2,11 +2,14 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "GruntConfiguration",
+        "MapSpec",
+        "UserSpec",
         "navigator"
     ],
     "modules": [
         "Grunt",
-        "fogger"
+        "fogger",
+        "test"
     ],
     "allModules": [
         {
@@ -17,6 +20,10 @@ YUI.add("yuidoc-meta", function(Y) {
             "displayName": "Grunt",
             "name": "Grunt",
             "description": "This Gruntfile specifies commands for our fogger project.\n\nRunning jasmine tests\n---------------------\nCurrent the jasmine test command is not working because\nour application runs on web2py. This would require a complex\nheadless environment that we might implement for the next iteration.\nFor now, tests can be run by navigating to a page and adding \"test=1\"\nas a GET URL parameter."
+        },
+        {
+            "displayName": "test",
+            "name": "test"
         }
     ]
 } };
